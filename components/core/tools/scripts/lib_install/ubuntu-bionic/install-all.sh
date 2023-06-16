51f69a2cf165e61e20cf5c158bb34e316d88036a
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-06-16 08:25:04 -0400

    Install fix

--- components/core/tools/scripts/lib_install/ubuntu-bionic/install-all.sh
+++ components/core/tools/scripts/lib_install/ubuntu-bionic/install-all.sh
@@ -2,5 +2,9 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-"$script_dir"/install-prebuilt-packages.sh
-"$script_dir"/install-packages-from-source.sh
+pushd "$script_dir"
+
+./install-prebuilt-packages.sh
+./install-packages-from-source.sh
+
+popd
