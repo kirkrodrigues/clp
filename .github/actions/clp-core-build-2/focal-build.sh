1918b4dcff97da6b537555814abfcc4f8ede2084
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-11-19 17:10:05 -0500

    Fix permissions issue.

--- .github/actions/clp-core-build-2/focal-build.sh
+++ .github/actions/clp-core-build-2/focal-build.sh
@@ -5,6 +5,5 @@ set -u
 
 pushd components/core || exit
 
-./tools/scripts/deps-download/download-all.sh
 cmake -B ./focal-build -DCMAKE_BUILD_TYPE=Release
 cmake --build ./focal-build --config Release
