cb51f22013696a0450e32ea3d6caee2efef5b1f3
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-04-23 15:41:06 -0400

    Use environment variables to build package container with current user; Minor refactoring.

--- tools/docker-images/clp-dependencies-x86-ubuntu-focal/build.sh
+++ tools/docker-images/clp-dependencies-x86-ubuntu-focal/build.sh
@@ -9,5 +9,10 @@ set -u
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 repo_root=${script_dir}/../../../
 
-docker build -t clp-dependencies-x86-ubuntu-focal:dev "${repo_root}" \
+docker build \
+    --build-arg CONTAINER_UID="${CONTAINER_UID:-$(id -u)}" \
+    --build-arg CONTAINER_GID="${CONTAINER_GID:-$(id -g)}" \
+    --build-arg CONTAINER_USER="${CONTAINER_USER:-${USER}}" \
+    --tag clp-dependencies-x86-ubuntu-focal:dev \
+    "${repo_root}" \
     --file "${script_dir}/Dockerfile"
