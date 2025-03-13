a9e1e360db39ef4e8c18ece51ede63417a955792
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-03-13 05:05:05 -0400

    Fix option name; Use correct header for off_t on macOS.

--- components/core/src/wrapped_facade_headers/sys/types.h
+++ components/core/src/wrapped_facade_headers/sys/types.h
@@ -3,10 +3,10 @@
 // IWYU pragma: end_exports
 
 // clang-format off
-#ifdef CLP_ENABLE_CLANG_TIDY_IWYU_EXPORTS
+#ifdef ENABLE_CLANG_TIDY_IWYU_EXPORTS
 // IWYU pragma: begin_exports
 #if defined(__APPLE__)
-#include <sys/_types.h>
+#include <sys/_types/_off_t.h>
 #endif
 // IWYU pragma: end_exports
 #endif
