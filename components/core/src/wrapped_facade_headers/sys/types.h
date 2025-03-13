4b575ba18b56eca718c5c1409deb71dc2d53d6b1
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2025-03-12 22:18:35 -0400

    Add wrapped facade header for sys/types.h on macOS.

---
+++ components/core/src/wrapped_facade_headers/sys/types.h
@@ -0,0 +1,13 @@
+// IWYU pragma: begin_exports
+#include <sys/types.h>
+// IWYU pragma: end_exports
+
+// clang-format off
+#ifdef CLP_ENABLE_CLANG_TIDY_IWYU_EXPORTS
+// IWYU pragma: begin_exports
+#if defined(__APPLE__)
+#include <sys/_types.h>
+#endif
+// IWYU pragma: end_exports
+#endif
+// clang-format on
